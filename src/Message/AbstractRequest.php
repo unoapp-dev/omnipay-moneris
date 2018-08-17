c8d36a83def493bd61d1f4814d43779f5a362b99
Anil Ahir <anilahir@unoindia.co>
2018-08-17 14:57:46 +0530

    first commit

---
+++ src/Message/AbstractRequest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace  Omnipay\Moneris\Message;
+
+abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
+{
+    protected $host = 'https://www3.moneris.com:443/gateway2/servlet/MpgRequest';
+    protected $testHost = 'https://esqa.moneris.com:443/gateway2/servlet/MpgRequest';
+    protected $endpoint = '';
+
+    public function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testHost : $this->host;
+    }
+
+    public function setEndpoint($endpoint)
+    {
+        $this->endpoint = $endpoint;
+    }
+
+    public function getMerchantId()
+    {
+        return $this->getParameter('merchantId');
+    }
+
+    public function setMerchantId($value)
+    {
+        return $this->setParameter('merchantId', $value);
+    }
+
+    public function getMerchantKey()
+    {
+        return $this->getParameter('merchantKey');
+    }
+
+    public function setMerchantKey($value)
+    {
+        return $this->setParameter('merchantKey', $value);
+    }
+
+    public function getPaymentMethod()
+    {
+        return $this->getParameter('payment_method');
+    }
+
+    public function setPaymentMethod($value)
+    {
+        return $this->setParameter('payment_method', $value);
+    }
+
+    public function getPaymentProfile()
+    {
+        return $this->getParameter('payment_profile');
+    }
+
+    public function setPaymentProfile($value)
+    {
+        return $this->setParameter('payment_profile', $value);
+    }
+
+    public function getOrderNumber()
+    {
+        return $this->getParameter('order_number');
+    }
+
+    public function setOrderNumber($value)
+    {
+        return $this->setParameter('order_number', $value);
+    }
+
+    protected function getHttpMethod()
+    {
+        return 'POST';
+    }
+
+    public function sendData($data)
+    {
+        $headers = [
+            'Content-Type' => 'application/xml'
+        ];
+        
+        if(!empty($data)) {
+            $httpResponse = $this->httpClient->request($this->getHttpMethod(), $this->getEndpoint(), $headers, $data);
+        }
+        else {
+            $httpResponse = $this->httpClient->request($this->getHttpMethod(), $this->getEndpoint(), $headers);
+        }
+    
+        try {
+            $xmlResponse = simplexml_load_string($httpResponse->getBody()->getContents());
+        }
+        catch (\Exception $e){
+            info('Guzzle response : ', [$httpResponse]);
+            $res = [];
+            $res['resptext'] = 'Oops! something went wrong, Try again after sometime.';
+            return $this->response = new Response($this, $res);
+        }
+
+        return $this->response = new Response($this, $xmlResponse);
+    }
+}
+
