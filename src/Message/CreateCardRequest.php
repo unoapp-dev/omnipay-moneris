0654d649210c73a6d30f37d8b415f0c8954e3b2e
Anil Ahir <anilahir@unoindia.co>
2018-08-23 13:31:21 +0530

    set optional params for create card

--- src/Message/CreateCardRequest.php
+++ src/Message/CreateCardRequest.php
@@ -17,16 +17,18 @@ class CreateCardRequest extends AbstractRequest
 
             $res_add_cc = $request->addChild('res_add_cc');
             $res_add_cc->addChild('cust_id', $this->getCard()->getBillingName());
-            $res_add_cc->addChild('phone', $this->getCard()->getBillingPhone());
+            $phone = $this->getCard()->getBillingPhone() != '' ? $this->getCard()->getBillingPhone() : 'NA';
+            $res_add_cc->addChild('phone', $phone);
             $res_add_cc->addChild('email', $this->getCard()->getEmail());
-            $res_add_cc->addChild('note', ' ');
+            $res_add_cc->addChild('note', 'NA');
             $res_add_cc->addChild('pan', $this->getCard()->getNumber());
             $res_add_cc->addChild('expdate', $this->getCard()->getExpiryDate('my'));
             $res_add_cc->addChild('crypt_type', 1);
 
             $avs_info = $res_add_cc->addChild('avs_info');
-            $avs_info->addChild('avs_street_number', ' ');
-            $avs_info->addChild('avs_street_name', $this->getCard()->getBillingAddress1());
+            $avs_info->addChild('avs_street_number', 'NA');
+            $avs_street_name = $this->getCard()->getBillingAddress1() != '' ? $this->getCard()->getBillingAddress1() : 'NA';
+            $avs_info->addChild('avs_street_name', $avs_street_name);
             $avs_info->addChild('avs_zipcode', $this->getCard()->getBillingPostcode());
 
             $data = $request->asXML();
