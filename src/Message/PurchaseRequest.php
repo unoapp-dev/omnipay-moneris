8ed0d9974209960a647293ea65f65780c528549e
Tymotheos Szulc <tim.e.szulc@gmail.com>
2021-03-19 15:17:00 -0400

    feat: add card payment with cvd verification

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -6,11 +6,43 @@ use Omnipay\Common\Exception\InvalidRequestException;
 
 class PurchaseRequest extends AbstractRequest
 {
+    /**
+     * CVD value is deliberately bypassed or is not provided by the merchant.
+     *
+     * @var int
+     * @see https://developer.moneris.com/en/Documentation/NA/E-Commerce%20Solutions/API/Purchase
+     */
+    const CVD_BYPASSED = 0;
+
+    /**
+     * CVD value is present.
+     *
+     * @var int
+     * @see https://developer.moneris.com/en/Documentation/NA/E-Commerce%20Solutions/API/Purchase
+     */
+    const CVD_PRESENT = 1;
+
+    /**
+     * CVD value is on the card, but is illegible.
+     *
+     * @var int
+     * @see https://developer.moneris.com/en/Documentation/NA/E-Commerce%20Solutions/API/Purchase
+     */
+    const CVD_ILLEGIBLE = 2;
+
+    /**
+     * Cardholder states that the card has no CVD imprint.
+     *
+     * @var int
+     * @see https://developer.moneris.com/en/Documentation/NA/E-Commerce%20Solutions/API/Purchase
+     */
+    const NO_CVD = 9;
+
     public function getData()
     {
         $data = null;
 
-        $this->validate('orderNumber', 'amount', 'paymentMethod');
+        $this->validate('orderNumber', 'cryptType', 'amount', 'paymentMethod');
 
         $paymentMethod = $this->getPaymentMethod();
 
@@ -32,7 +64,32 @@ class PurchaseRequest extends AbstractRequest
                 $data = $request->asXML();
                 break;
 
-            // Todo: card & token payment
+            case 'card':
+                $this->validate('card');
+
+                $request = new \SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><request></request>');
+                $request->addChild('store_id', $this->getMerchantId());
+                $request->addChild('api_token', $this->getMerchantKey());
+
+                $card = $this->getCard();
+
+                $purchase = $request->addChild('purchase');
+                $purchase->addChild('pan', $card->getNumber());
+                $purchase->addChild('expdate', $card->getExpiryDate('ym'));
+                $purchase->addChild('order_id', $this->getOrderNumber());
+                $purchase->addChild('cust_id', 'Transaction_'.$this->getOrderNumber());
+                $purchase->addChild('amount', $this->getAmount());
+                $purchase->addChild('crypt_type', $this->getCryptType());
+                $purchase->addChild('dynamic_descriptor', $this->getDescription());
+
+                $cvd_info = $purchase->addChild('cvd_info');
+                $cvd_info->addChild('cvd_indicator', self::CVD_PRESENT);
+                $cvd_info->addChild('cvd_value', $card->getCvv());
+
+                $data = $request->asXML();
+                break;
+
+            // Todo: token payment
 
             default:
                 throw new InvalidRequestException('Invalid payment method');
